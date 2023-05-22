807879923a1d9bc40264c7845802d83a032e2623
Ricardo Velez <ricardovelecal@gmail.com>
2023-05-22 17:23:24 -0600

    test 9

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 const io = require('socket.io')(server, {
     cors: {
-        origin: ['http://localhost:4200']
+        origin: ['https://nimble-axolotl-ddafb5.netlify.app']
     }
 });
 
